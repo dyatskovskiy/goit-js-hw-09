97f3fbf237f5465074e78d2e8ec397ed91d1d816
dyatskovskiy <d.yatskovskiy@ukr.net>
2023-09-15 22:38:57 +0300

    add date picker feature

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,18 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import 'dist/notiflix-notify-aio-3.2.6.min.js';
+import Notiflix from 'notiflix';
+// import 'dist/notiflix-3.2.6.min.css';
+
+//Фіча вибору дати і часу для початку відліку
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr('#datetime-picker', options);
