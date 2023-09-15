a852fc1de5f20bc6ffd64ba5a512dafe335b320e
dyatskovskiy <d.yatskovskiy@ukr.net>
2023-09-15 22:29:58 +0300

    add notiflix lib

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,5 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'dist/notiflix-notify-aio-3.2.6.min.js';
