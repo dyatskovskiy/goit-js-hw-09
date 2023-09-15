62bc8c5069659e22b6c337c216d52d572e539e21
dyatskovskiy <131163877+dyatskovskiy@users.noreply.github.com>
2023-09-15 19:01:47 +0000

    Deploying to gh-pages from @ dyatskovskiy/goit-js-hw-09@70b983a62e694067655fbc231ffc2c44b8974b57 🚀

---
+++ 01-color-switcher.263cad36.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.body;let d=null;function a(){r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.addEventListener("click",(function(){d=setInterval(a,1e3),t.setAttribute("disabled","true"),e.removeAttribute("disabled","true")})),e.addEventListener("click",(function(){clearInterval(d),t.removeAttribute("disabled","true"),e.setAttribute("disabled","true")})),e.setAttribute("disabled","true");
+//# sourceMappingURL=01-color-switcher.263cad36.js.map
