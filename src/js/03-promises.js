3fd933991f859d812fb494959745df4efc9671f6
dyatskovskiy <d.yatskovskiy@ukr.net>
2023-09-18 09:25:33 +0300

    3rd hw in progress

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,54 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'notiflix/dist/notiflix-notify-aio-3.2.6.min.js';
+
+const createBtn = document.querySelector('.form > button');
+const firstDelayInput = document.querySelector('input[name=delay]');
+const delayStepInput = document.querySelector('input[name=step]');
+const amountInput = document.querySelector('input[name=amount]');
+
+// const position = amountInput.value;
+// const delay = delayStepInput.value;
+const firstDelay = firstDelayInput.value;
+
+createBtn.addEventListener('click', e => {
+  e.preventDefault();
+  const amount = amountInput.value;
+  const delay = delayStepInput.value;
+
+  setTimeout(() => {
+    setTimeout(() => {
+      createPromise(position, delay)
+        .then(({ position, delay }) => {
+          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+    }, delay);
+  }, firstDelay);
+});
+
+const createPromise = (position, delay) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+
+      if (shouldResolve) {
+        resolve({ position, delay });
+      }
+
+      reject({ position, delay });
+    }, delay);
+  });
+};
+// Напиши скрипт, який на момент сабміту форми викликає функцію createPromise(position, delay) стільки разів, скільки ввели в поле amount. Під час кожного виклику передай їй номер промісу (position), що створюється, і затримку, враховуючи першу затримку (delay), введену користувачем, і крок (step).
+
+// Доповни код функції createPromise таким чином, щоб вона повертала один проміс, який виконується або відхиляється через delay часу. Значенням промісу повинен бути об'єкт, в якому будуть властивості position і delay зі значеннями однойменних параметрів. Використовуй початковий код функції для вибору того, що потрібно зробити з промісом - виконати або відхилити.
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
